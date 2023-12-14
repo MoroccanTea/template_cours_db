5bea7619209315588382204221fe157a3f098135
MoroccanTea <hamza@essad.ma>
2023-12-14 14:23:01 +0100

    switch to ejs

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
-var authenticate = require('../middlewares/authenticate');
 
 /* GET home page. */
-router.get('/', authenticate, function(req, res, next) {
+router.get('/', function(req, res, next) {
   res.render('index', { title: 'Ecomm API' });
 });
 
